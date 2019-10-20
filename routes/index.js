9a2fc33a1edcfccd9a35915435ef15425af0ac88
Adam Feirick <afeirick96@gmail.com>
2019-10-20 12:27:12 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json  ([{
+                name: 'Adam',
+                 age: 23,
+                 friend: {name: 'Jeremy1',age: 23,}
+            }]);
+});
+
+module.exports = router;
